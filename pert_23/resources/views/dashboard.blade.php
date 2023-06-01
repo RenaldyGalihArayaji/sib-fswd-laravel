25850533e6b70cbfc7ca5d727b3d1da58c963445
Renaldy Galih Arayaji <redanredan46@gmail.com>
2023-06-01 15:45:54 +0700

    pertemuan 23

---
+++ pert_23/resources/views/dashboard.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.main')
+
+@section('content')
+    <h1>Halaman Dashboard</h1>
+    <div class="row">
+        <div class="col-md-3">
+            <ul class="list-group">
+                <li class="list-group-item"><a href="" class="text-decoration-none">Slider</a></li>
+                <li class="list-group-item"><a href="" class="text-decoration-none">Kategori Produk</a></li>
+                <li class="list-group-item"><a href="" class="text-decoration-none">Group Pengguna</a></li>            
+                <li class="list-group-item"><a href="" class="text-decoration-none">Daftar Pengguna</a></li>            
+              </ul>
+        </div>
+        <div class="col-md-9">
+            
+        </div>
+        
+    </div>
+@endsection
